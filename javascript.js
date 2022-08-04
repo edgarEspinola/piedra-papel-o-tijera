da0dada82b5a4452a1b28f75c8adb700667fdaa6
edgar espinola <edgar.espinoladavid@gmail.com>
2022-08-03 22:33:37 -0300

    se realizan funciones

--- javascript.js
+++ javascript.js
@@ -1,41 +1,52 @@
 let jugaor = 0
 let min = 1
 let max = 3
-function aleatorio(min,max){
+
+function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+function eleccion(jugada) {
+    resultado = ""
+    if (jugada == 1) {
+        resultado = "piedra 🥌 "
+    } else if (jugada == 2) {
+        resultado = "papel 🧻"
+    } else if (jugada == 3) {
+        resultado = "elegiste ✂ "
+    } else {
+        resultado = "elejiste perder!"
+    }
+    return resultado
+}
 //math.floor-entero--math.random-aleatorio
-let pc = aleatorio(1,3)
-let jugador = prompt("elije 1 para piedra, 2 para papel , 3 para tijera ")
-//alert("elegiste " + jugador )
-if(jugador ==1){
-    alert("elegiste 🥌 ")
-}else if(jugador ==2){
-    alert("elegiste 🧻")
-}else if(jugador == 3){
-    alert("elegiste ✂ ")
-}else{
-    alert("elejiste perder!")
-}
-if(pc ==1){
-    alert("pc elije 🥌 ")
-}else if(pc ==2){
-    alert("pc elije 🧻")
-}else if(pc == 3){
-    alert("pc elije ✂ ")
-}
-//combate
-if(pc == jugador){
-    alert("empate!")
-}else if (jugador == 1 && pc == 3){
-    alert("ganaste!")
-}else if(jugador == 2 && pc == 1){
-    alert("ganaste!")
-}else if(jugador == 3 && pc == 2){
-    alert("ganaste!")   
-}
-else{
-    alert("perdiste!")
-}
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+while (triunfos < 3 && perdidas < 3) {
+    pc = aleatorio(1, 3)
+    let jugador = prompt("elije 1 para piedra, 2 para papel , 3 para tijera ")
+    //alert("elegiste " + jugador )
 
+    alert("pc elije: " + eleccion(pc))
+    alert("tu elijes: " + eleccion(jugador))
 
+    //combate
+    if (pc == jugador) {
+        alert("empate!")
+    } else if (jugador == 1 && pc == 3) {
+        alert("ganaste!")
+        triunfos += 1
+    } else if (jugador == 2 && pc == 1) {
+        alert("ganaste!")
+        triunfos += 1
+    } else if (jugador == 3 && pc == 2) {
+        alert("ganaste!")
+        triunfos += 1
+    } else {
+        alert("perdiste!")
+        perdidas += 1
+    }
+
+}
+alert("ganaste " + triunfos + "veces. perdiste " + perdidas + "veces")
